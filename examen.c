7ff73f64141169c28011377bc1fe9d150f0fb9ae
Ronny Quispe <ronny.quispe@udla.edu.ec>
2024-01-24 15:23:14 -0500

    agregamos la estructura para cada ingreso de producto

--- examen.c
+++ examen.c
@@ -1,12 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 // Definimos  la estructura producto
 struct producto {
     char codigo[25];
     char nomproduc[50];
     char descproduc[50];
     int cantidad;
-    float precioCompra;
+    float precioCompra//
+};
+void ingresarDatos(struct producto *productos, int n) {// funcion ingresar datos 
+    for (int i = 0; i < n; i++) {
+        printf("ingrese los datos del producto  %d:\n", i + 1);// ingresa  los dats del producto #
+        
+
+        printf("codigo  del producto: ");
+        gets(productos[i].codigo);
+        fflush(stdin);// Limpiar el buffer del teclado
+
+        printf("Nombre del producto: ");
+        gets(productos[i].nomproduc);
+        fflush(stdin);// Limpiar el buffer del teclado
+
+        printf("Descuento de producto: ");
+        gets(productos[i].descproduc);
+        fflush(stdin);// Limpiar el buffer del teclado
+
+
+        printf("precio compra: ");
+        scanf("%f", &(productos[i].precioCompra));
+        fflush(stdin);// Limpiar el buffer del teclado
+
+        printf("cantidad: ");
+        scanf("%d", &(productos[i].cantidad));
+        fflush(stdin); // Limpiar el buffer del teclado
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
