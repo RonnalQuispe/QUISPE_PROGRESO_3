d16d514ef750a16354ddb4abe0d82dfa24eb6558
Ronny Quispe <ronny.quispe@udla.edu.ec>
2024-01-24 15:00:27 -0500

    estructura del producto

---
+++ examen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Definimos  la estructura producto
+struct producto {
+    char codigo[25];
+    char nomproduc[50];
+    char descproduc[50];
+    int cantidad;
+    float precioCompra;
+}
+
+
+
+
